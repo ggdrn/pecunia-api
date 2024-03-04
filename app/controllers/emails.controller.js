39b341f426f0ef0bf2620cc9af65ece734848d98
Gustavo Garcia <gustavo.garcia@oliveiratrust.com.br>
2024-03-04 13:33:44 -0300

    feat: Adicionado leitura de anexo em png para obter mais informações

--- app/controllers/emails.controller.js
+++ app/controllers/emails.controller.js
@@ -1,6 +1,7 @@
 const gmail = require("../config/gmail.config")
 const processaConta = require("../service/genimi.service")
 
+
 function setPartEmail(payload) {
 	try {
 		if (payload.parts[0].body.data) {
@@ -14,12 +15,31 @@ function setPartEmail(payload) {
 		return ""
 	}
 }
+async function converterPdf2Img(pdfBuffer) {
+	try {
+		const pdfToPng = require('pdf-to-png-converter').pdfToPng;
 
+
+		const pngPage = await pdfToPng(pdfBuffer, {
+			disableFontFace: false,
+			useSystemFonts: false,
+			pagesToProcess: [1],
+			viewportScale: 2.0,
+			pdfFilePassword: '15777'
+		});
+
+		return pngPage[0].content;
+	} catch (error) {
+		return null;
+	}
+
+
+}
 exports.findAll = async (req, res) => {
 	try {
 
 		// Crie a consulta de pesquisa
-		const query = 'Enel';
+		const query = 'enel';
 
 		// Faça a requisição para buscar emails
 		const response = await gmail.users.messages.list({
@@ -29,30 +49,55 @@ exports.findAll = async (req, res) => {
 		// Acessar os emails na propriedade "messages" da resposta
 		const messages = response.data.messages;
 
-		// Extrair o título e o conteúdo de cada email
-		// res.send({messages});
-
-		// const result = await messages.map(async message => {]
 		const allData = [];
-		for (let i = 0; i < 3; i++) {
-		// for (let i = 0; i < messages.length; i++) {
+		for (let i = 0; i < 5; i++) {
+		// for (const message of messages) {
 			const { data } = await gmail.users.messages.get({
 				userId: 'me',
 				id: messages[i].id
+				// id: message.id
 			})
 			const subject = data.payload.headers.find(header => header.name === 'Subject').value;
-			const body = Buffer.from(setPartEmail(data.payload), 'base64').toString();
-			const result = await processaConta({ subject, body })
-			allData.push(result);
-			//   const subject = message.payload.headers.find(header => header.name === 'Subject').value;
-			//   const content = message.payload.body.data;
+			let body = Buffer.from(setPartEmail(data.payload), 'base64');
+			if (body){
+				body = body.toString()
+			}
+			let anexo = "";
+			
+			// Processar anexos, se existirem
+			if (data.payload.parts && data.payload.parts.length > 1) {
+				const attachments = data.payload.parts.slice(1); // Ignorar a parte do corpo
 
-			//   return {
-			// 	subject,
-			// 	content
-			//   };
-			// });
+				for (const attachment of attachments) {
+					const attachmentId = attachment.body.attachmentId;
+
+					if (attachmentId) {
+						// Obter detalhes do anexo
+						const attachmentDetails = await gmail.users.messages.attachments.get({
+							userId: 'me',
+							messageId: messages[i].id,
+							// messageId: message.id,
+							id: attachmentId,
+						});
+
+						const attachmentData = Buffer.from(attachmentDetails.data.data, 'base64')
+						// Salvar o anexo
+
+						anexo = await converterPdf2Img(attachmentData);
+						if (anexo) {
+							anexo = anexo.toString("base64")
+						}
+					}
+				}
+			}
+			console.log("processando email: ",   messages[i].id)
+			// allData.push({ subject, body, anexo });
+			const result = await processaConta({ subject, body }, anexo)
+			allData.push(result);
+			console.log("processamento concluido")
+
 		}
+		console.log("finalizado")
 		res.send({ result: allData });
 	} catch (err) {
 		res.status(500).send({
--- app/service/genimi.service.js
+++ app/service/genimi.service.js
@@ -1,7 +1,27 @@
-const processaConta = async (payload) => {
+const processamentoDados = async json => {
+    // Remover caracteres indesejados
+    const cleanJsonString = json
+        .replace(/\n/g, '') // Remover quebras de linha
+        .replace(/\\/g, '') // Remover barras invertidas
 
-    const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
+    // Remover o código de formatação Markdown ```json
+    const cleanJsonStringWithoutMarkdown = cleanJsonString
+        .replace(/^.*?```json|```$/g, '');
 
+    try {
+        // Converter a string limpa para um objeto JavaScript
+        const jsonObject = JSON.parse(cleanJsonStringWithoutMarkdown);
+        return jsonObject;
+    } catch (error) {
+        console.error('Erro ao converter a string para JSON:', error.message);
+        return cleanJsonStringWithoutMarkdown;
+    }
+}
+
+const processamentoJsonGemini = async json => {
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    // const MODEL_NAME = "gemini-pro";
     const MODEL_NAME = "gemini-pro";
     const API_KEY = process.env.GENIMI_KEY;
 
@@ -15,65 +35,93 @@ const processaConta = async (payload) => {
         maxOutputTokens: 2048,
     };
 
-    const safetySettings = [
-        // {
-        //     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        // },
-        // {
-        //     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        // },
-        // {
-        //     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        // },
-        // {
-        //     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        // },
-    ];
-
     const parts = [
         {
-            text: `De acordo com os dados que vou te passar, você sabe me dizer que se trata de uma conta de luz, água, ou internet? 
-                Sabe me dizer data dessa conta, no formato yyyy-mm-dd? 
-                Qual valor da dessa conta? Consegue me passar o código de barras dessa conta ou o código pix? 
-                Essa resposta você me retorna em uma estrutura de JSON, segue o exemplo de estrutura de dado que deve retornar:
-                 
-                { 
-                    tipo_conta: "agua" ou "luz" ou "internet", 
-                    valor: 222, 
-                    data_conta: 2022-01-01, 
-                    tipo_pagamento: "pix" ou "boleto" ou false para caso não ache, 
-                    cod_pagamento: 123412414124214 ou false para caso não ache.
-                }
-
-                segue o json abaixo para analise:
-
-                ${JSON.stringify(payload)}
-
-                Caso você não consiga processar todos os dados, retorne o json de exemplo com todos os campos nulos.
-                `
-
-        },
-    ];
+            text: `
+            PEDIDO: eu tenho essa string aqui : ${json} \n
+            Quero convertela para  um objeto em js. Quero que você remover esses "\n" e esse "\"  e qualquer caractere que quebre o json do texto para ter uma estrutura de json limpa, SAIDA: Apenas o json limpo`
+        }
+    ]
 
     const result = await model.generateContent({
         contents: [{ role: "user", parts }],
         generationConfig,
         // safetySettings,
     });
-    // Remover caracteres de quebra de linha
-    const cleanStr = result.response.text()
-    .replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":')
-    .replace(/'/g, '"');
 
-    // Converter a string para um objeto JSON
-    const jsonData = JSON.parse(cleanStr);
+    try {
+        return JSON.parse(result.response.text())
+    }
+    catch (e) {
+        console.log("Impossivel limpar o dado fornecido")
+        return result.response.text()
+    }
 
-    // return cleanStr;
-    return jsonData;
+
+}
+
+const processaConta = async (payload, imageFormat) => {
+
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    // const MODEL_NAME = "gemini-pro";
+    const MODEL_NAME = imageFormat ? "gemini-pro-vision" : "gemini-pro";
+    const API_KEY = process.env.GENIMI_KEY;
+
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+    const generationConfig = {
+        temperature: 0.9,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+    };
+
+    const parts = [
+        {
+            text: `
+                PEDIDO: \n
+                - De acordo com os dados de texto de um corpo de email que vou te passar, você sabe me dizer que se trata de uma conta de luz, água, ou internet ou cartão?  \n
+                - Sabe me dizer data dessa conta, no formato yyyy-mm-dd? \n
+                - Qual valor da dessa conta? Consegue me passar o código de barras dessa conta ou o código pix? \n 
+                CONTEXTO, exemplo de uma SAIDA, seguinte a estrutura:
+                {
+                    tipo_conta: TYPE:STRING // ("agua" ou "luz" ou "internet" ou "cartao"), 
+                    tipo_cartao: TYPE:STRING // (Se for tipo cartão, falar de qual banco esse cartão de crédito se trata ou false para caso não ache)
+                    valor: TYPE:NUMBER, // Valor da conta que precisa ser pago
+                    data_conta: TYPE:DATE // (yyyy-mm-dd), 
+                    tipo_pagamento: TYPE:STRING // ("pix" ou "boleto" ou false para caso não ache), 
+                    cod_pagamento: TYPE:STRING // (codigo para pagar o pix ou o codigo de barras do boleto ou false para caso não ache.)
+                } 
+                \n
+                PEDIDO:Quero uma estrutura válida de JSON sem o texto marcando 'json' na resposta, Caso você não consiga processar todos os dados, retorne o json de exemplo com todos os campos nulos. Os seguintes dados para serem analisados:
+                \n
+                ${JSON.stringify(payload)}
+                ${imageFormat ? 'analise a imagem em anexo para descobrir as informações solicitadas' : ''}
+                `
+        },
+
+    ];
+    if (imageFormat) {
+        parts.push({
+            inlineData: {
+                mimeType: "image/png",
+                data: imageFormat
+            }
+        })
+    }
+
+    const result = await model.generateContent({
+        contents: [{ role: "user", parts }],
+        generationConfig,
+        // safetySettings,
+    });
+
+    const obj = await processamentoDados(result.response.text());
+
+    return obj;
+
 
 }
 
